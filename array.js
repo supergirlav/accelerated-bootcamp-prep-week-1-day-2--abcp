cbe391125f706fbea81f62e044c91fcea87b4813
Aleks Vino <aleks.vino@gmail.com>
2018-01-20 04:48:04 +0000

    Done.

--- array.js
+++ array.js
@@ -1,34 +1,44 @@
 function addElementToBeginningOfArray(array, element) {
  // add the element to the beginning of the array without mutating the array
+return [element, ...array]
+  
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
+array.unshift(element);
+return.array
 }
 
 function addElementToEndOfArray(array, element) {
-
+return[...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+array.push(element)
+return array
 }
 
 function accessElementInArray(array, index) {
-
+return array[index]
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
+array.shift(element)
+return array
+  
 }
 
 function removeElementFromBeginningOfArray(array) {
+return array.slice(1)
+  
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-
+array.pop(element)
+return array
 }
 
 function removeElementFromEndOfArray(array) {
-
+return array.slice(0, array.lenght - 1)
 }
--- if.js
+++ if.js
@@ -4,5 +4,12 @@ if (pokemon == "Pikachu") {
   console.log("I choose you Pikachu!!")
 }
 
+var getIt = "10"
 
+if (n <= getIt) {
+  console.log("n")}
+  else if (n >= getIt) {
+    console.log("nah, ah!")
+  }
 
+var getIt(n) {
